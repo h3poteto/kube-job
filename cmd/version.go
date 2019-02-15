5dc5cc57479645d97a5c86fdf20cdb3cd850c073
AkiraFukushima <h3.poteto@gmail.com>
2019-02-16 01:04:00 +0900

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func versionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("kube-job v0.1.0")
+		},
+	}
+
+	return cmd
+}
