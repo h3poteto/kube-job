6b524f99a47d665f1816b40c54045ce15179ff37
Yu Shigetani <s2g4t1n2@gmail.com>
2021-03-25 22:03:46 +0900

    Support InClusterConfig

--- pkg/job/config.go
+++ pkg/job/config.go
@@ -1,11 +1,24 @@
 package job
 
 import (
+	"os"
+
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func newClient(configFile string) (*kubernetes.Clientset, error) {
+	_, err := os.Stat(configFile)
+	if err != nil {
+		kubeConfig, err := rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		return kubernetes.NewForConfig(kubeConfig)
+	}
+
 	kubeConfig, err := clientcmd.BuildConfigFromFlags("", configFile)
 	if err != nil {
 		return nil, err
