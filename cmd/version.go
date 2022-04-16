f54bfa31183266dfc1408fc38424fa1776cbd012
AkiraFukushima <h3.poteto@gmail.com>
2022-04-16 15:10:34 +0900

    Bump to version 0.6.0

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ func versionCmd() *cobra.Command {
 		Use:   "version",
 		Short: "Print the version number",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("kube-job v0.5.3")
+			fmt.Println("kube-job v0.6.0")
 		},
 	}
 
